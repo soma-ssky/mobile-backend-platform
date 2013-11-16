53eb687b79a6ee492aa4091c42cbf64986eaf3d1
System Administrator <root@taehoon-ui-MacBook-Pro.local>
2013-11-16 19:43:00 +0900

    bug fix

--- src/main/java/me/ssky/util/NoMatchHandler.java
+++ src/main/java/me/ssky/util/NoMatchHandler.java
@@ -7,7 +7,6 @@ public class NoMatchHandler implements Handler<HttpServerRequest> {
 
 	@Override
 	public void handle(HttpServerRequest request) {
-		System.out.println(request.path());
 		request.response().setStatusCode(400).setStatusMessage("Bad Request").end();
 	}
 
